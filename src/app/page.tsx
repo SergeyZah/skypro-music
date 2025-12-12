e241d9dbaba6a3fa7dfc5786dabb7fcd49fdae10
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-12-12 12:05:49 +0300

    hw-1: вынес модуль "Navigate"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,15 @@
 import './page.css';
 import Image from 'next/image';
 import Link from 'next/link';
+import Navigate from './components/Navigate/Navigate';
+import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className={'wrapper'}>
-      <div className={'container'}>
-        <main className={'main'}>
-          <nav className={'main__nav'}>
-            <div className={'nav__logo'}>
-              <Image
-                width={250}
-                height={170}
-                className={'logo__image'}
-                src="/img/logo.png"
-                alt={'logo'}
-              />
-            </div>
-            <div className={'nav__burger'}>
-              <span className={'burger__line'}></span>
-              <span className={'burger__line'}></span>
-              <span className={'burger__line'}></span>
-            </div>
-            <div className={'nav__menu'}>
-              <ul className={'menu__list'}>
-                <li className={'menu__item'}>
-                  <Link href="#" className={'menu__link'}>
-                    Главное
-                  </Link>
-                </li>
-                <li className={'menu__item'}>
-                  <Link href="#" className={'menu__link'}>
-                    Мой плейлист
-                  </Link>
-                </li>
-                <li className={'menu__item'}>
-                  <Link href="../signin.html" className={'menu__link'}>
-                    Войти
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <Navigate/>
           <div className={'centerblock'}>
             <div className={'centerblock__search'}>
               <svg className={'search__svg'}>
