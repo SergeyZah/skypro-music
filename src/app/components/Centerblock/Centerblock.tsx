ba428199fee289f252d31dbad1c47aff7ad5b358
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-12-16 23:07:36 +0300

    hw-1: вынес компонент "Content"

--- src/app/components/Centerblock/Centerblock.tsx
+++ src/app/components/Centerblock/Centerblock.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import styles from './centerblock.module.css';
 import classnames from 'classnames';
 import Filter from '../Filter/Filter';
+import Content from '../Content/Content';
 
 export default function Centerblock() {
   return (
@@ -20,22 +21,7 @@ export default function Centerblock() {
       <h2 className={styles.centerblock__h2}>Треки</h2>
       <Filter />
       <div className={styles.centerblock__content}>
-        <div className={styles.content__title}>
-          <div className={classnames(styles.playlistTitle__col, styles.col01)}>
-            Трек
-          </div>
-          <div className={classnames(styles.playlistTitle__col, styles.col02)}>
-            Исполнитель
-          </div>
-          <div className={classnames(styles.playlistTitle__col, styles.col03)}>
-            Альбом
-          </div>
-          <div className={classnames(styles.playlistTitle__col, styles.col04)}>
-            <svg className={styles.playlistTitle__svg}>
-              <use xlinkHref="/img/icon/sprite.svg#icon-watch"></use>
-            </svg>
-          </div>
-        </div>
+        <Content />
         <div className={styles.content__playlist}>
           <div className={styles.playlist__item}>
             <div className={styles.playlist__track}>
