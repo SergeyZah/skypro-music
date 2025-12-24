36f0e84adcb4ba3eac8b87fb76f5af2df00f6276
Sergey_Zaharychev <zaharychevs@yandex.ru>
2025-12-24 15:27:37 +0300

    hw-2: добавил компонент "Tracklist"

--- src/app/components/Centerblock/Centerblock.tsx
+++ src/app/components/Centerblock/Centerblock.tsx
@@ -1,9 +1,7 @@
 import styles from './centerblock.module.css';
-import Filter from '../Filter/Filter';
-import Track from '../Track/Track';
+import Filter from '../Filter/Filter'; 
 import Search from '../Search/Search';
-import { data } from '@/data';
-import Listheader from '../Listheader/Listheader';
+import Tracklist from '../Tracklist/Tracklist';
 
 export default function Centerblock() {
   return (
@@ -11,22 +9,7 @@ export default function Centerblock() {
       <Search />
       <h2 className={styles.centerblock__h2}>Треки</h2>
       <Filter />
-      <div className={styles.centerblock__content}>
-        <Listheader />
-        <div className={styles.content__playlist}>
-          {data.map((track) => {
-            return (
-              <Track
-                key={track._id}
-                name={track.name}
-                author={track.author}
-                album={track.album}
-                time={track.duration_in_seconds}
-              />
-            );
-          })}
-        </div>
-      </div>
+      <Tracklist />
     </div>
   );
 }
