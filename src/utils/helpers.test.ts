6ca9136b408fd6f9c391f9790af48922ebbde07d
Sergey_Zaharychev <zaharychevs@yandex.ru>
2026-02-09 22:47:11 +0300

    hw-6: тестирование чистой функции formatTime

---
+++ src/utils/helpers.test.ts
@@ -0,0 +1,13 @@
+import { formatTime } from './helpers';
+
+describe('formatTime', () => {
+  it('Добавление нуля, если секунд < 10', () => {
+    expect(formatTime(62)).toBe('1:02');
+  });
+  it('Форматирует время < 1 минуты', () => {
+    expect(formatTime(27)).toBe('0:27');
+  });
+  it('Обрабатывает 0 секунд', () => {
+    expect(formatTime(0)).toBe('0:00');
+  });
+});
